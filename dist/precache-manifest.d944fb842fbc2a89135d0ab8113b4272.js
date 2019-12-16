c1ff432baa4eb9f03db5009a8161130288b02fb1
CatarinaBatista <49941212+CatarinaBatista@users.noreply.github.com>
2019-12-16 10:35:01 +0000

    Add files via upload

---
+++ dist/precache-manifest.d944fb842fbc2a89135d0ab8113b4272.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74339614d84abe2e87437649cfb2f27a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90d707c6a4b93ee8cb6f",
+    "url": "main.css"
+  },
+  {
+    "revision": "90d707c6a4b93ee8cb6f",
+    "url": "main.js"
+  }
+]);
